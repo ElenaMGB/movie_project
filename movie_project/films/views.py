46b47d8de6f5f3cf237e083eb120d7cf6841bf61
ElenaM <eldev24@gmail.com>
2025-02-11 05:49:31 +0100

    add structure, migrations

--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,3 +1,24 @@
+from django.shortcuts import render, redirect
+from .forms import CommentForm
+from .models import Comment
+
+
+def add_comment(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('comments_list')
+    else:
+        form = CommentForm()
+    return render(request, 'films/add_comment.html', {'form': form})
+
+
+def comments_list(request):
+    films = Comment.objects.all()
+    return render(request, 'films/comments_list.html', {'comment': comment})
+
+
 from django.shortcuts import render
 
 # Create your views here.
